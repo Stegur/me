e7fa6fe3f0f5da238f695906e9f0ffb1f689781a
Alex Stegura <stegura.alex@gmail.com>
2018-07-02 23:51:39 +0300

    Пукнкт второй

--- homework_4.php
+++ homework_4.php
@@ -3,12 +3,26 @@
 $continents = [
     'Africa' => ['Giraffa', 'Hippopotamus', 'Hippotigris'],
     'Europe' => ['Genetta genetta', 'Balaenoptera edeni', 'Alces alces'],
-    'Asia' => ['Panthera tigris tigris ', 'Naemorhedus goral', 'Elephas maximus'],
+    'Asia' => ['Panthera tigris tigris', 'Naemorhedus goral', 'Elephas maximus'],
     'Antarctica' => ['Pygoscelis antarctica', 'Leptonychotes weddellii'],
     'North America' => ['Rangifer tarandus', 'Lepus arcticus', 'Gulo gulo'],
     'South America' => ['Hydrochoerus hydrochaeris', 'Furipterus horrens'],
-    'Australia' => ['Furipterus horrens', 'Phascolarctos cinereus', 'Tachyglossidae '],
+    'Australia' => ['Ornithorhynchus anatinus', 'Phascolarctos cinereus', 'Tachyglossidae'],
 ];
 
+
+foreach ($continents as $continent => $animals)
+{
+    foreach ($animals as $animal)
+    {
+        $twoWordsAnimal = explode(' ', $animal);
+        if (count($twoWordsAnimal) == 2){                   //исключил 'Panthera tigris tigris', у которого 3 слова
+            $twoWordsAnimalArray [] = $animal;
+//            echo '<pre>';
+//            //print_r($twoWordsAnimalArray);
+        }
+    }
+}
 echo '<pre>';
-print_r($continents);
+print_r($twoWordsAnimalArray);
+

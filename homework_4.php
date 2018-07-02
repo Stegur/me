110e214b878a071def6ea551052a6c14b44101fb
Alex Stegura <stegura.alex@gmail.com>
2018-07-02 21:31:39 +0300

    Пукнкт первый

---
+++ homework_4.php
@@ -0,0 +1,14 @@
+<?php
+
+$continents = [
+    'Africa' => ['Giraffa', 'Hippopotamus', 'Hippotigris'],
+    'Europe' => ['Genetta genetta', 'Balaenoptera edeni', 'Alces alces'],
+    'Asia' => ['Panthera tigris tigris ', 'Naemorhedus goral', 'Elephas maximus'],
+    'Antarctica' => ['Pygoscelis antarctica', 'Leptonychotes weddellii'],
+    'North America' => ['Rangifer tarandus', 'Lepus arcticus', 'Gulo gulo'],
+    'South America' => ['Hydrochoerus hydrochaeris', 'Furipterus horrens'],
+    'Australia' => ['Furipterus horrens', 'Phascolarctos cinereus', 'Tachyglossidae '],
+];
+
+echo '<pre>';
+print_r($continents);

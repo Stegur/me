caf399b7fe9a974705c3c4a27d358bfbf4e961d5
Alex Stegura <stegura.alex@gmail.com>
2018-07-04 23:04:06 +0300

    Вычислил что для работы с JSON не нужно использовать foreach... спустя 5 часов

--- homework_5.php
+++ homework_5.php
@@ -4,29 +4,8 @@ $file = 'berlin.json';
 $fileContent = file_get_contents($file);
 $json = json_decode($fileContent, 1);
 
-echo '<pre>';
-
-foreach ($json as $key => $section) {
-//    print_r($key);
-//    print_r($section);
-//    echo '<br>';
-    if (is_array($section)) {
-        foreach ($section as $param => $value) {
-//            var_dump($param);
-            if (is_array($value)) {
-                foreach ($value as $numbers => $number) {
-//                    var_dump($numbers);
-//                    var_dump($number);
-                }
-            } else {
-//                var_dump($value);
-            }
-        }
-    } else {
-//        var_dump($section);
-    }
-}
-
 $tempK = $json['main']['temp'];
 $tempC = $tempK - 273.15;
-echo $tempC . '°C';
+
+$humidity = $json['main']['humidity'];
+echo  'Температура в ' . $json['name'] . ' = ' . $tempC . '°C';

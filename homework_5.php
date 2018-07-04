d11af86b5c0c8c7cb83c942b9d7c4787863eb6da
Alex Stegura <stegura.alex@gmail.com>
2018-07-04 20:07:23 +0300

    Черновик, получил температуру

---
+++ homework_5.php
@@ -0,0 +1,31 @@
+<?php
+$file = 'berlin.json';
+
+$fileContent = file_get_contents($file);
+$json = json_decode($fileContent, 1);
+
+echo '<pre>';
+
+foreach ($json as $key => $section) {
+//    print_r($key);
+//    print_r($section);
+    echo '<br>';
+    if (is_array($section)) {
+        foreach ($section as $param => $value) {
+//            var_dump($param);
+            if (is_array($value)) {
+                foreach ($value as $numbers => $number) {
+//                    var_dump($numbers);
+//                    var_dump($number);
+                }
+            } else {
+//                var_dump($value);
+            }
+        }
+    } else {
+//        var_dump($section);
+    }
+}
+
+
+echo $json['main']['temp'];

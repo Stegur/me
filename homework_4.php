a29028edb05b4e28c7325b0c1c29c7a8d1b19b90
Alex Stegura <stegura.alex@gmail.com>
2018-07-05 10:24:51 +0300

    Озарение! Переделка кода без тоны дополнительного кода. 4 пункта из 5

--- homework_4.php
+++ homework_4.php
@@ -10,83 +10,21 @@ $continents = [
     'Australia' => ['Ornithorhynchus anatinus', 'Phascolarctos cinereus', 'Tachyglossidae'],
 ];
 
+echo '<pre>';
+
 
 foreach ($continents as $continent => $animals)
 {
+    echo '<h2>' . $continent . '</h2>';
     foreach ($animals as $animal)
-    {
-        $twoWordsAnimal = explode(' ', $animal);
+    {   $twoWordsAnimal = explode(' ', $animal);
+        //var_dump($twoWordsAnimal);
 
         if (count($twoWordsAnimal) == 2){                   //исключил 'Panthera tigris tigris', у которого 3 слова
-            $secondWordArray [] = $twoWordsAnimal[1];
-            shuffle($secondWordArray);
-            $firstWordArray [] = $twoWordsAnimal[0];
-            //shuffle($firstWordArray);
+            echo  $animal . '<br/>';
         }
     }
 }
-
-
-
-echo '<h2>Europe</h2>';
-for ($i = 0; $i < count($firstWordArray); $i++)
-{
-    if($firstWordArray [$i] == 'Genetta' or $firstWordArray [$i] == 'Balaenoptera' or $firstWordArray [$i] == 'Alces')
-    {
-        echo $firstWordArray [$i] . ' ' . $secondWordArray [$i] .'<br/>';
-        }
-}
-
-echo '<h2>Asia</h2>';
-for ($i = 0; $i < count($firstWordArray); $i++)
-{
-    if($firstWordArray [$i] == 'Naemorhedus' or $firstWordArray [$i] == 'Elephas')
-    {
-        echo $firstWordArray [$i] . ' ' . $secondWordArray [$i] .'<br/>';
-    }
-}
-
-echo '<h2>Antarctica</h2>';
-for ($i = 0; $i < count($firstWordArray); $i++)
-{
-    if($firstWordArray [$i] == 'Pygoscelis' or $firstWordArray [$i] == 'Leptonychotes')
-    {
-        echo $firstWordArray [$i] . ' ' . $secondWordArray [$i] .'<br/>';
-    }
-}
-
-echo '<h2>North America</h2>';
-for ($i = 0; $i < count($firstWordArray); $i++)
-{
-    if($firstWordArray [$i] == 'Rangifer' or $firstWordArray [$i] == 'Lepus' or $firstWordArray [$i] == 'Gulo')
-    {
-        echo $firstWordArray [$i] . ' ' . $secondWordArray [$i] .'<br/>';
-    }
-}
-
-echo '<h2>South America</h2>';
-for ($i = 0; $i < count($firstWordArray); $i++)
-{
-    if($firstWordArray [$i] == 'Hydrochoerus' or $firstWordArray [$i] == 'Furipterus')
-    {
-        echo $firstWordArray [$i] . ' ' . $secondWordArray [$i] .'<br/>';
-    }
-}
-
-echo '<h2>Australia</h2>';
-for ($i = 0; $i < count($firstWordArray); $i++)
-{
-    if($firstWordArray [$i] == 'Ornithorhynchus' or $firstWordArray [$i] == 'Phascolarctos')
-    {
-        $result = $firstWordArray [$i] . ' ' . $secondWordArray [$i] . '<br/>';
-        echo $result;
-    }
-}
-
-
-
-
-
 
 
 

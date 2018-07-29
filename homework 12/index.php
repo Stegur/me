9611026312e197f978c6ec60e9cd6aed8a2f9993
Alex Stegura <stegura.alex@gmail.com>
2018-07-29 23:57:04 +0300

    Расширенное задание

--- homework 12/index.php
+++ homework 12/index.php
@@ -1,19 +1,32 @@
 <?php
-$connect = mysqli_connect("localhost","root", "", "libreria");
-$sql = "SELECT * from books";
+$connect = mysqli_connect("localhost", "root", "", "libreria");
 
-$res = mysqli_query($connect, $sql);
-$data = mysqli_fetch_assoc($res);
+// TODO опробовать перебрать Case
 
-//foreach ($res as $rows) {
-//    echo '<pre>';
-//
-//    var_dump($rows);
+if (array_key_exists('isbn', $_GET ) && $_GET['isbn'] != NULL) {
+    $isbn = strip_tags($_GET['isbn']);
+    $sql = "SELECT * FROM books WHERE isbn='{$isbn}'";
+} elseif (array_key_exists('name', $_GET) && $_GET['name'] != NULL) {
+    $name = strip_tags($_GET['name']);
+    $sql = "SELECT * FROM books WHERE name LIKE '%{$name}%'";
+} elseif (array_key_exists('author', $_GET) && $_GET['author'] != NULL) {
+    $author = strip_tags($_GET['author']);                  //todo striptags()
+    $sql = "SELECT * FROM books WHERE author LIKE '%{$author}%'";
+} else {
+    $sql = "SELECT * from books";
+}
 
-//}
+if (!$res = mysqli_query($connect, $sql)) {
+    echo 'Не правильный запрос';
+} else {
+    $data = mysqli_fetch_assoc($res);
+}
+//strip_tags()
+//echo "<pre>";
+//var_dump($author);
+//var_dump($name);
+//var_dump($isbn);
 
-//var_dump($res);
-//var_dump($data);
 ?>
 
 
@@ -29,38 +42,53 @@ $data = mysqli_fetch_assoc($res);
             border-collapse: collapse;
             border-spacing: 0;
         }
+
         thead {
             background: #dfdfdf;
             text-align: center;
             font-weight: bold;
         }
+
         td {
-            padding: 3px;
+            padding: 5px;
             border: solid 1px gray;
         }
     </style>
 </head>
 <body>
+<h1>Библиотека успешного человека</h1>
+<form action="index.php" method="get">
+    <input type="text" placeholder="ISBN" name="isbn">
+    <input type="text" placeholder="Название книги" name="name">
+    <input type="text" placeholder="Автор книги" name="author">
+    <input type="submit" value="Поиск">
+</form>
+<br>
 <table>
     <thead>
     <tr>
-    <td>Название</td>
-    <td>Автор</td>
-    <td>Год выпуска</td>
-    <td>Жанр</td>
-    <td>ISBN</td>
+        <td>Название</td>
+        <td>Автор</td>
+        <td>Год выпуска</td>
+        <td>Жанр</td>
+        <td>ISBN</td>
     </tr>
     </thead>
     <tbody>
     <?php foreach ($res as $row) : ?>
-    <tr>
-        <td><?php echo $row['name']?></td>
-        <td><?php echo $row['author']?></td>
-        <td><?php echo $row['year']?></td>
-        <td><?php echo $row['genre']?></td>
-        <td><?php echo $row['isbn']?></td>
-    </tr>
-    <?php endforeach; ?>
+        <tr>
+            <td><?php echo $row['name']?></td>
+            <td><?php echo $row['author']?></td>
+            <td><?php echo $row['year']?></td>
+            <td><?php echo $row['genre']?></td>
+            <td><?php echo $row['isbn']?></td>
+        </tr>
+    <?php endforeach;
+    if (!$data) {
+        echo "По Вашему запросу ничего не найдено. Вы можете уточнить запрос или обобразить все доступные книги<br>";
+        echo "<a href='index.php'>Отобразить?</a><br>";
+    }
+    ?>
     </tbody>
 </table>
 

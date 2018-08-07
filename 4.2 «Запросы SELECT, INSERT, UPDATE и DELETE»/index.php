76ac3b50a418aab070c45ae7e3e0b7f3e1af74ec
Alex Stegura <stegura.alex@gmail.com>
2018-08-07 15:08:58 +0300

    Задание для самых отчаянных

--- 4.2 «Запросы SELECT, INSERT, UPDATE и DELETE»/index.php
+++ 4.2 «Запросы SELECT, INSERT, UPDATE и DELETE»/index.php
@@ -6,7 +6,17 @@ define('DB_NAME', 'todo');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-$sql = "SELECT * FROM tasks";
+if (array_key_exists('list', $_POST)) {
+    if ($_POST['list'] == 'date') {
+        $sql = "SELECT * FROM tasks ORDER BY date_added";
+    } elseif ($_POST['list'] == 'done') {
+        $sql = "SELECT * FROM tasks ORDER BY is_done";
+    } elseif ($_POST['list'] == 'description') {
+        $sql = "SELECT * FROM tasks ORDER BY description";
+    }
+} else {
+    $sql = "SELECT * FROM tasks";
+}
 
 $connect_str = DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME;
 $db = new PDO($connect_str, DB_USER, DB_PASS);
@@ -14,7 +24,6 @@ $db = new PDO($connect_str, DB_USER, DB_PASS);
 $result = $db->query($sql);
 
 $error_array = $db->errorInfo();
-
 if ($db->errorCode() != 0000) {
     echo "SQL ошибка: " . $error_array[2] . '<br>';
 }
@@ -39,7 +48,7 @@ if (array_key_exists('action', $_GET)) {
 }
 
 //echo '<pre>';
-//var_dump($_GET);
+//var_dump($_POST);
 
 ?>
 

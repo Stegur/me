d3f7a4d0db696a6c5f3ba175673d415726fa935e
Alex Stegura <stegura.alex@gmail.com>
2018-08-09 14:17:04 +0300

    Тупик

--- 4.3 «SELECT из нескольких таблиц»/core/db_enter.php
+++ 4.3 «SELECT из нескольких таблиц»/core/db_enter.php
@@ -1,1 +1,29 @@
 <?php
+
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'todo');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+if (array_key_exists('list', $_POST)) {
+    if ($_POST['list'] == 'date') {
+        $sql = "SELECT * FROM tasks ORDER BY date_added";
+    } elseif ($_POST['list'] == 'done') {
+        $sql = "SELECT * FROM tasks ORDER BY is_done";
+    } elseif ($_POST['list'] == 'description') {
+        $sql = "SELECT * FROM tasks ORDER BY description";
+    }
+} else {
+    $sql = "SELECT * FROM tasks";
+}
+
+$connect_str = DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME;
+$db = new PDO($connect_str, DB_USER, DB_PASS);
+
+$result = $db->query($sql);
+
+$error_array = $db->errorInfo();
+if ($db->errorCode() != 0000) {
+    echo "SQL ошибка: " . $error_array[2] . '<br>';
+}
--- 4.3 «SELECT из нескольких таблиц»/core/register.php
+++ 4.3 «SELECT из нескольких таблиц»/core/register.php
@@ -1,5 +1,31 @@
 <?php
+require_once '../core/db_enter.php';
 
+if (array_key_exists('login', $_POST) && array_key_exists('pass', $_POST)) {
+    $login = strip_tags($_POST['login']);
+    $pass = md5(strip_tags($_POST['pass']));
+}
+
+if (array_key_exists('reg', $_POST)) {
+    $sql = "SELECT * FROM user where login='" . $login . "'";
+    $is_user = $db->query($sql);
+    foreach ($is_user as $user) {
+        if ($user['login'] == $login) {
+            echo "<p>Такой пользователь уже существует в базе данных.</p>";
+        }
+    }
+    if () { // что вставить как условие?
+        $salt = "I don`t know how to use PHP";
+        $password = md5($salt . $pass);
+        $sql = "INSERT INTO user(`id`, `login`, `password`) VALUES (null ,'" . $login . "','" . $password . "')";
+        echo $sql;
+    }
+} else {
+    echo "<p>Введите данные для регистрации или войдите, если уже регистрировались:</p>";
+}
+
+//echo '<pre>';
+//var_dump($_POST);
 ?>
 
 <!doctype html>
@@ -9,10 +35,10 @@
     <title>Вход</title>
 </head>
 <body>
-<p>Введите данные для регистрации или войдите, если уже регистрировались:</p>
-<form action="../index.php" method="post">
-    <input type="text" name="login"> <input type="password" name="pass"> <input type="submit" value="Вход"> <input
-            type="submit" value="Регистрация">
+
+<form action="register.php" method="post">
+    <input type="text" name="login" placeholder="Логин"> <input type="password" name="pass" placeholder="Пароль">
+    <input type="submit" value="Вход" name="enter"> <input type="submit" value="Регистрация" name="reg">
 </form>
 </body>
 </html>
